b742d46cef96818eab88625c328bd4bcca04b414
Norris Nakagaki <norrisnakagaki@gmail.com>
2018-02-07 22:45:37 -0500

    wip

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const publicClient = new Gdax.PublicClient();
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
